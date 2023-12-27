ab34bf28c917934347c9f00543af7e12a486ba1d
Mateusz Mudry <matmudry@gmail.com>
2023-12-28 00:33:31 +0100

    gradient added

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ function waterAmount() {
   resultDiv.textContent = weight + " ml";
 
   let bottleDiv = document.getElementById("bottle");
-  bottleDiv.textContent = (weight / 1500).toFixed(1) + " butelek wody 1,5 l";
+  bottleDiv.textContent = (weight / 1500).toFixed(1);
 
   let glassDiv = document.getElementById("glass");
-  glassDiv.textContent = (weight / 350).toFixed(1) + " szklanek 350 ml";
+  glassDiv.textContent = (weight / 350).toFixed(1);
 
   let cupDiv = document.getElementById("cup");
-  cupDiv.textContent = (weight / 200).toFixed(1) + " kubków 200 ml";
+  cupDiv.textContent = (weight / 200).toFixed(1);
 }
 
 let inputElement = document.getElementById("my-weight");
