66f839818698a342500a45098f65d6781a6b773d
Mateusz Mudry <matmudry@gmail.com>
2024-01-10 01:17:24 +0100

    result animation and reset button added

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
+function animateNumber(element, targetNumber) {
+  let currentNumber = 0;
+  const animationSpeed = 10;
+  const updateInterval = 2;
+
+  const update = () => {
+    if (currentNumber < targetNumber) {
+      currentNumber += Math.ceil(
+        (targetNumber - currentNumber) / animationSpeed
+      );
+      element.textContent = currentNumber + " ML";
+      setTimeout(update, updateInterval);
+    } else {
+      element.textContent = targetNumber + " ML";
+    }
+  };
+
+  update();
+}
+
 function waterAmount() {
   const weightInput = document.getElementById("my-weight");
   let weight = weightInput.value * 30;
 
+  // let resultDiv = document.getElementById("result");
+  // resultDiv.textContent = weight + " ml";
+
   let resultDiv = document.getElementById("result");
-  resultDiv.textContent = weight + " ml";
+  resultDiv.textContent = "0 ML";
+  animateNumber(resultDiv, weight);
 
   let bottleDiv = document.getElementById("bottle");
   bottleDiv.textContent = (weight / 1500).toFixed(1);
